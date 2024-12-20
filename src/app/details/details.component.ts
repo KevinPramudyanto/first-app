c52ecedb60fbe4b0cbe8ab5c0ca059cd9ca02571
KevinPramudyanto <kevinpramudyanto92@gmail.com>
2024-12-20 16:16:09 +0800

    Create routing

---
+++ src/app/details/details.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-details',
+  imports: [],
+  template: `<p>details works!</p> `,
+  styleUrl: './details.component.css',
+})
+export class DetailsComponent {}
